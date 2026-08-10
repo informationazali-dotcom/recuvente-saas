d10a264152a7955b2b11c1c0de1a4e8408f3c2fb
informationazali-dotcom <informationazali@gmail.com>
2026-08-10 20:52:09 +0200

    Update confirmer-paiement.js

--- confirmer-paiement.js
+++ confirmer-paiement.js
@@ -1,4 +1,4 @@
- import { createClient } from "@supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js";
 
 const supabaseAdmin = createClient(
   process.env.VITE_SUPABASE_URL,
